bd3fc7f06d2cc25bb6cb0c5a5d253f6039af8a61
luizpkey <tecpdu@gmail.com>
2022-08-11 21:37:49 -0300

    <Issue> Refactor script. Insert feedback to user

--- IaC/IaC.sh
+++ IaC/IaC.sh
@@ -1,5 +1,7 @@
+echo "Limpando pasta/Cleaning folders"
 sudo rm -rf /publico /adm /ven /sec
-pause
+
+echo "Excluindo Usuários/Remove users"
 sudo userdel carlos 
 sudo userdel maria
 sudo userdel joao
@@ -9,13 +11,18 @@ sudo userdel roberto
 sudo userdel josefina
 sudo userdel amanda
 sudo userdel rogerio
+
+echo "Excluindo Grupos/Remove groups"
 sudo groupdel GRP_ADM 
 sudo groupdel GRP_VEN 
 sudo groupdel GRP_SEC
 
+echo "Criando Grupoos/Create groups"
 sudo groupadd GRP_ADM
 sudo groupadd GRP_VEN
 sudo groupadd GRP_SEC
+
+echo "Criando Usuários/Create users"
 sudo useradd carlos     -c "Carlos Brasil"     -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_ADM 
 sudo useradd maria      -c "Maria Brasil"      -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_ADM 
 sudo useradd joao       -c "Joao Brasil"       -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_ADM 
@@ -25,7 +32,11 @@ sudo useradd roberto    -c "Roberto Brasil"    -s /bin/bash -p $(openssl passwd 
 sudo useradd josefina   -c "Josefina Brasil"   -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_SEC 
 sudo useradd amanda     -c "Amanda Brasil"     -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_SEC 
 sudo useradd rogerio    -c "Rogerio Brasil"    -s /bin/bash -p $(openssl passwd -crypt 123mudar) -G GRP_SEC 
+
+echo "Criando diretórios/Create folders"
 sudo mkdir /publico /adm /ven /sec
+
+echo "Alterando privilégios/Privileges management"
 sudo chmod 777 /publico
 sudo chmod 770 /adm /ven /sec
 sudo chown root:GRP_ADM /adm/
